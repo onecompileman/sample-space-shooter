8f12d846eaf9f703e3121494f540a86d275897fc
Stephen Vinuya <stephen.vinuya@ffuf.de>
2018-12-12 14:30:46 +0800

    refactor (asset-manager): looping array

--- scripts/asset-manager.js
+++ scripts/asset-manager.js
@@ -5,8 +5,8 @@ class AssetManager {
         this.bulletImg = loadImage('assets/images/bullet.png');
         this.starImg = loadImage('assets/images/star.png');
 
-        this.enemiesImg = Array.from(Array(20).keys()).map(a => loadImage(`assets/images/enemies/${a + 1}.png`));
-        this.meteorsImg = Array.from(Array(2).keys()).map(a => loadImage(`assets/images/meteors/${a + 1}.png`));
+        this.enemiesImg = [...Array(20).keys()].map(a => loadImage(`assets/images/enemies/${a + 1}.png`));
+        this.meteorsImg = [...Array(2).keys()].map(a => loadImage(`assets/images/meteors/${a + 1}.png`));
     }
 
 }
